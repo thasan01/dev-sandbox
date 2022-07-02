4e99c31aa7699f7f38a50aa451cd3c0fb1ab18e2
thasan01 <thasan2004@yahoo.com>
2022-07-02 12:09:09 -0400

    spring-webapp: initial commit.

---
+++ spring-webapp/src/main/java/com/codingronin/spring/webapp/api/model/http/v1/ApiBaseRequest.java
@@ -0,0 +1,14 @@
+package com.codingronin.spring.webapp.api.model.http.v1;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+/**
+ * This is a placeholder for the other requests
+ */
+public class ApiBaseRequest {
+}
---
+++ spring-webapp/src/main/java/com/codingronin/spring/webapp/api/model/http/v1/CreateUser.java
@@ -0,0 +1,19 @@
+package com.codingronin.spring.webapp.api.model.http.v1;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class CreateUser {
+  @NotNull
+  String userName;
+
+  @Email
+  String email;
+}
