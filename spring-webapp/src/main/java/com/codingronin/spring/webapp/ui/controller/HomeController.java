9b5547fe6d917b5ea096a3e0f666a074502e503e
thasan01 <hasanto1@US1198724W1.mshome.net>
2022-07-22 18:03:16 -0400

    added JSP required dependency

--- spring-webapp/src/main/java/com/codingronin/spring/webapp/ui/controller/HomeController.java
+++ spring-webapp/src/main/java/com/codingronin/spring/webapp/ui/controller/HomeController.java
@@ -3,13 +3,14 @@ package com.codingronin.spring.webapp.ui.controller;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
   @GetMapping
-  public String home(Map<String, Object> model) {
-    return "index";
+  public ModelAndView home(Map<String, Object> model) {
+    return new ModelAndView("index");
   }
 
 }
--- spring-webapp/src/main/webapp/WEB-INF/view/index.jsp
+++ spring-webapp/src/main/webapp/WEB-INF/view/index.jsp
@@ -8,9 +8,7 @@
 	<div>
 		<div>
 			<h1>Spring Boot JSP Example</h1>
-			<h2>Hello ${message}</h2>
-			
-			Click on this <strong><a href="next">link</a></strong> to visit another page.
+			<h2>Hello ${message}</h2>			
 		</div>
 	</div>
 </body>
