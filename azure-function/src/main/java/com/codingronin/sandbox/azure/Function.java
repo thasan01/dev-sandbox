9ade7b60c330043957ea77785641abb56db6c212
thasan01 <thasan2004@yahoo.com>
2023-08-14 22:29:45 -0400

    error handling

--- azure-function/src/main/java/com/codingronin/sandbox/azure/Function.java
+++ azure-function/src/main/java/com/codingronin/sandbox/azure/Function.java
@@ -1,6 +1,8 @@
 package com.codingronin.sandbox.azure;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -15,6 +17,8 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
  */
 public class Function {
 
+  static final String ERROR_RESP_TEXT = "Encountered an error while processing the request. ";
+
   @FunctionName("Ping")
   public HttpResponseMessage ping(//
       @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST},
@@ -23,17 +27,23 @@ public class Function {
       final ExecutionContext context//
   ) //
   {
-    context.getLogger().info("Java HTTP trigger processed a request.");
+    Logger log = context.getLogger();
+    log.info("Java HTTP trigger processed a request.");
 
-    // Parse query parameter
-    final String query = request.getQueryParameters().get("name");
-    final String name = request.getBody().orElse(query);
+    try {
+      // Parse query parameter
+      final String query = request.getQueryParameters().get("name");
+      final String name = request.getBody().orElse(query);
 
-    if (name == null) {
-      return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
-          .body("request is missing name in url").build();
-    } else {
+      if (name == null)
+        throw new IllegalArgumentException("Request is missing name in url");
+
       return request.createResponseBuilder(HttpStatus.OK).body("Service is up." + name).build();
+
+    } catch (Exception ex) {
+      log.log(Level.SEVERE, ERROR_RESP_TEXT, ex);
+      return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+          .body(ERROR_RESP_TEXT + ex.getMessage()).build();
     }
   }
 }
